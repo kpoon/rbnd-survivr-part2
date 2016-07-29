16e426be560d2df8d7e9d2bb910df4451e0312f6
Kelly Markiewicz <kellypoon@gmail.com>
2016-07-28 22:00:54 -0600

    Commit changes to jury files

---
+++ lib/jury.rb
@@ -0,0 +1,32 @@
+class Jury
+	attr_accessor :members
+
+	def initialize
+		@members = []
+	end	
+
+	def add_member(member)
+		@members << member
+	end	
+
+	def cast_votes(finalists)
+		jury_vote = Hash.new(0)
+		@members.each do |member|
+			finalist = finalists.sample
+			jury_vote[finalist] += 1
+			puts "#{member.to_s.capitalize} voted for #{finalist.to_s.capitalize}."
+		end	
+  		return jury_vote
+	end	
+
+	def report_votes(final_votes)
+		final_votes.each do |key, value|
+			puts key.name.capitalize + ' received ' + value.to_s + ' votes.'
+		end	
+	end	
+
+	def announce_winner(final_votes)
+		final_votes.max_by{|k, v| v}[0]
+	end	
+end
+
