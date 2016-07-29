4d47f32c1a259fdde57df04c94b8bc91bceae6cd
Kelly Markiewicz <kellypoon@gmail.com>
2016-07-29 07:17:37 -0600

    Modified tribe.rb file to pass testing

---
+++ lib/tribe.rb
@@ -0,0 +1,24 @@
+class Tribe
+	attr_accessor :name, :members
+
+	def initialize(options={})
+		@name = options[:name]
+		@members = options[:members]
+
+		puts "#{@name} tribe"
+	end	
+
+	def to_s
+		@name
+	end
+
+  def tribal_council(options={})
+    #one memeber is immune
+    immune = options[:immune]
+    #need to vote one person off (randomly) but keep immune member
+    voted_off_list = @members.select {|member| member != immune}
+    voted_off = voted_off_list.sample
+    @members.delete(voted_off)
+    return voted_off
+  end
+end
